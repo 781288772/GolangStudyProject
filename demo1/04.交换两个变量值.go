0aaa89cf722059942cbfef41256da8954a39dbb5
chy <781288772@qq.com>
2021-12-19 20:46:02 +0800

    交换两个变量位置加强版

--- demo1/04.交换两个变量值.go
+++ demo1/04.交换两个变量值.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2, temp int
-	num1 = 10
-	num2 = 20
-	temp = num1
-	num1 = num2
-	num2 = temp
+	//var num1, num2, temp int
+	//num1 = 10
+	//num2 = 20
+	//temp = num1
+	//num1 = num2
+	//num2 = temp
+	num1 := 10
+	num2 := 20
+
+	//将num2 取出 赋值给num1
+	num1, num2 = num2, num1
 
 	fmt.Println(num1, num2)
 
