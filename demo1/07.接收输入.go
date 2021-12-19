a7f7b6f27e6d9579016408355f6e766ad21dc850
chy <781288772@qq.com>
2021-12-19 21:17:08 +0800

    接收输入

--- demo1/07.接收输入.go
+++ demo1/07.接收输入.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	var age int
 	fmt.Printf("请输入年龄")
-	fmt.Scanf("%d", &age) //通过scanf函数将键盘输入的数据赋值给变量，但变量前必须加取地址符号&
-	fmt.Println("age=", age)
+	//fmt.Scanf("%d", &age) //通过scanf函数将键盘输入的数据赋值给变量，但变量前必须加取地址符号&
+	//fmt.Println("age=", age)
+
+	fmt.Scan(&age)
+	fmt.Print("age=", age)
 
 }
