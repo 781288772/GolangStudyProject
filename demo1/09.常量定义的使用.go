2210cd32f8f58c1eda57ebcb74f5cbe345714b69
chy <781288772@qq.com>
2021-12-20 22:57:23 +0800

    算数运算符

--- demo1/09.常量定义的使用.go
+++ demo1/09.常量定义的使用.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	//const PI float64 = 3.14
 	const PI = 3.14
-	//fmt.Printf("%.2f", PI)
-	fmt.Print(PI)
+	fmt.Printf("%.2f", PI)
+	//fmt.Print(PI)
 
 }
---
+++ demo1/10.算数运算符.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1 = 20
+	var num2 = 10
+
+	fmt.Println("num1+num2=", num1+num2)
+	fmt.Println("num1-num2=", num1-num2)
+	fmt.Println("num1*num2=", num1*num2)
+	fmt.Println("num1/num2=", num1/num2)
+
+}
