bea8b2cd812783406b67251a4c15ca03c2c4dea4
chy <781288772@qq.com>
2021-12-23 22:47:44 +0800

    连接数据库

--- demo2/example.go
+++ demo2/example.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
 func main() {
+	db, _ := sql.Open("mysql", "root:root@(127.0.0.1:3306)/mysql")
+
+	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		fmt.Println("数据库连接失败")
+		return
+	}
+	fmt.Println("数据库连接成功")
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
