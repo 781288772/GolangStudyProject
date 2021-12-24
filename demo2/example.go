9b5ac1077c23f251488fbc7663e95c5b3d60257d
chy <781288772@qq.com>
2021-12-24 15:50:33 +0800

    用户查询接口初成

--- demo2/example.go
+++ demo2/example.go
@@ -10,6 +10,16 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+type sqlResponse struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
 func main() {
 	db, _ := sql.Open("mysql", "root:root@(127.0.0.1:3306)/test")
 
@@ -20,35 +30,40 @@ func main() {
 		return
 	}
 	// fmt.Println("数据库连接成功")
-	var id string
-	var username string
-
-	rows, err := db.Query("select id,username from sys_user")
-	for rows.Next() {
-
-		err := rows.Scan(&id, &username)
-		if err != nil {
-			log.Fatal("scan failed: ", err)
-		}
-
-		log.Printf("id: %s username:%s\n", id, username)
-	}
-
-	// fmt.Print(rows)
-	if err != nil {
-		fmt.Print(err)
-		return
-	}
+	// var uid string
+	// var uusername string
 
 	r := gin.Default()
 
 	r.GET("/list", func(c *gin.Context) {
+		resUser := make([]User, 0)
+		rows, err := db.Query("select id,username from sys_user")
 
-		c.JSON(http.StatusOK, gin.H{
-			"status":  "200",
-			"message": "查询成功",
-			"data":    rows,
-		})
+		for rows.Next() {
+			var userTemp User
+			err := rows.Scan(&userTemp.Id, &userTemp.Username)
+			if err != nil {
+				log.Fatal("scan failed: ", err)
+			}
+			log.Printf("id: %s username:%s\n", userTemp.Id, userTemp.Username)
+
+			// log.Print("temp", userTemp)
+			resUser = append(resUser, userTemp)
+		}
+		log.Print("data", resUser)
+
+		// fmt.Print(rows)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		c.JSON(http.StatusOK, resUser)
+
+		// c.JSON(http.StatusOK, gin.H{
+		// 	"status":  "200",
+		// 	"message": "查询成功",
+		// 	"data":    resUser,
+		// })
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
