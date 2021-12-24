571c01bde919269bd3b381026455bcd72065bc25
这该死的魅力 <llb@163.com>
2021-12-24 10:47:07 +0800

    连接mysql

--- demo2/example.go
+++ demo2/example.go
@@ -3,13 +3,25 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"net/http"
 )
 
 func main() {
-	db, _ := sql.Open("mysql", "root:root@(127.0.0.1:3306)/mysql")
+	initDB()
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello gin",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func initDB() {
+	db, _ := sql.Open("mysql", "root:root@(127.0.0.1:3306)/test")
 
 	defer db.Close()
 	err := db.Ping()
@@ -18,11 +30,7 @@ func main() {
 		return
 	}
 	fmt.Println("数据库连接成功")
-	r := gin.Default()
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello gin",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	row, err := db.Query("select *from sys_user")
+	fmt.Print(row)
+
 }
