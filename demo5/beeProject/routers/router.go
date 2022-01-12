5535ceebad9dbfdf3c7df0f7752bce30037d7bc6
chy <781288772@qq.com>
2022-01-12 23:27:24 +0800

    orm插入数据

--- demo5/beeProject/routers/router.go
+++ demo5/beeProject/routers/router.go
@@ -6,7 +6,8 @@ import (
 )
 
 func init() {
-	beego.Router("/", &controllers.MainController{}, "get:showGet")
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/insert", &controllers.MainController{}, "get:showGet")
 	////给请求指定自定义方法，一个请求一个方法
 	//beego.Router("/login", &controllers.LoginController{}, "get:showLogin;post:postFunc")
 	////多个请求一个方法
